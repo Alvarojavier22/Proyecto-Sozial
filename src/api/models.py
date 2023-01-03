48329f75e33a50732bc3972b4ab151dc34708df6
Alvaro Javier <alvarojavierchagas@hotmail.com>
2023-01-03 14:36:08 +0000

    Ajustes de usuario

--- src/api/models.py
+++ src/api/models.py
@@ -2,14 +2,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Signup(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    surname = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<Signup {self.email}>'
 
     def serialize(self):
         return {
@@ -17,3 +19,4 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
