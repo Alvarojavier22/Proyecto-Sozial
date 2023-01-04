befea98297e77e3972c5058c26d687c436eb351f
alejandropmz <alejandropacheco2497@gmail.com>
2023-01-04 02:03:59 +0000

    first commit

--- src/api/models.py
+++ src/api/models.py
@@ -3,8 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Signup(db.Model):
+    __tablename__ = "signup"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
+    name = db.Column(db.String(50), unique=False, nullable=False)
     surname = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
@@ -17,6 +18,50 @@ class Signup(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "name":self.name,
+            "surname":self.surname,
+            "email":self.email,
+            "is_active":self.is_active
+        }
+
+class Products(db.Model):
+    __tablename__ = "products"
+    id = db.Column(db.Integer, primary_key=True)
+    categories = db.Column(db.String(50), nullable=False)
+    product_id = db.Column(db.Integer, unique=True, nullable=False)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    description = db.Column(db.String(300), unique=False, nullable=False)
+    price = db.Column(db.Integer, unique=False, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    avaliable = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Products {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "categories":self.categories,
+            "product_id": self.product_id,
+            "name": self.name,
+            "description": self.description,
+            "price": self.price,
+            "quantity":self.quantity,
+            "avaliable": self.avaliable
+        }
+
+class Categories(db.Model):
+    __tablename__ = "categories"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    products_categories = db.Column(db.String(100), db.ForeignKey('products.categories'))
+    user = db.relationship(Products)
+
+    def __repr__(self):
+        return f'<Categories {self.name}>'
+
+    def serialize(self):
+        return {
+            "name": self.name
         }
 
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,17 +2,139 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, Signup
+from api.models import db, Signup, Products, Categories
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
+@api.route('/social/', methods=['GET'])
+def social():
+    return jsonify({
+        "msg":"social screen"
+    })
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+# SIGNUP ############################################
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+@api.route('/signup/', methods=['POST'])
+def signup():
+    email = request.json.get("email")
+    password = request.json.get("password")
+    is_active = request.json.get("is_active")
+    name = request.json.get("name")
+    surname = request.json.get("surname")
+    signup = Signup(email = email, password = password, is_active = is_active, name = name, surname = surname)
+    users = Signup.query.filter(Signup.email == email).first()
 
-    return jsonify(response_body), 200
+    if not users is None:
+
+        return jsonify({
+            "msg":"User already exist"
+        }), 404
+
+    db.session.add(signup)
+    db.session.commit()
+    print(signup)
+
+    return jsonify({
+        "msg":"User create succefully"
+    }), 201
+
+# ALL USERS
+
+@api.route('/users/', methods=['GET'])
+def users():
+    users = Signup.query.filter(Signup.__tablename__ == "signup").all()
+    all_users = []
+
+    if users is None:
+        return jsonify({
+            "msg":"No have any user"
+        }), 404
+
+    for i in range(len(users)):
+        all_users.append(users[i].serialize())
+
+    if len(all_users) > 0:
+        return jsonify({
+            "users":all_users
+        }), 201
+
+
+# CATEGORIAS
+
+@api.route('/products/categories/', methods=['GET'])
+def categories():
+    categories = Categories.query.filter(Categories.__tablename__ == "categories").all()
+    all_categories = []
+
+    for i in range(len(categories)):
+        all_categories.append(categories[i].serialize())
+
+    if len(categories) > 0:
+        return jsonify({
+            "Categories":all_categories
+        }), 201
+
+
+    return jsonify({
+        "msg":"No categories avaliables"
+    }), 404
+
+
+
+# TODOS LOS PRODUCTOS
+
+@api.route('/products/', methods=['GET'])
+def products():
+    products = Products.query.filter(Products.__tablename__ == "products").all()
+    all_products = []
+
+    for i in range(len(products)):
+        all_products.append(products[i].serialize())
+
+    if len(products) > 0:
+        return jsonify({
+            "Products":all_products
+        }), 201
+
+
+    return jsonify({
+        "msg":"No products avaliables"
+    }), 404
+
+
+# CADA PRODUCTO
+
+@api.route('/products/<int:product_id>/')
+def each_product(product_id):
+    product = Products.query.filter(Products.product_id == product_id).first()
+
+    if not product is None:
+        return jsonify({
+            "product":product.serialize()
+        }), 201
+
+    return jsonify({
+        "msg":"product not found"
+    }), 401
+
+""" # PRODUCTOS POR CATEGORÍAS HACER DE ÚLTIMAS
+
+@api.route('/products/categories/<string:categorie>/')
+def categories_products(categorie):
+    products_categories = Products.query.filter(Products.categories == categorie).all()
+    all_products_categories = []
+
+    if products_categories is None:
+        return jsonify({
+            "msg":"No have products in this categorie"
+        }), 404
+
+    
+    for i in range(len(products_categories)):
+        if products_categories[i].categories == "tecnologia":
+            
+
+ """
+
+
