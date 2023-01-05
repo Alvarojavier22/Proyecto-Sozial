83aacbe880fa7c7a944ec7b1770e95ba3aae8d0c
Alvaro Javier <alvarojavierchagas@hotmail.com>
2023-01-05 13:02:26 +0000

    TockenBlocklist solucionado

--- src/api/models.py
+++ src/api/models.py
@@ -62,3 +62,8 @@ class Categories(db.Model):
             "name": self.name
         }
 
+class TokenBlocklist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    jti = db.Column(db.String(36), nullable=False, index=True)
+    created_at = db.Column(db.DateTime, nullable=False)
+
