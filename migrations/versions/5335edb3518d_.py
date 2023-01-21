723a8049b0e98dd7089bc7d8251f736ec69bd18b
alejandropmz <alejandropacheco2497@gmail.com>
2023-01-21 01:55:14 +0000

    errores de route corregidos

--- migrations/versions/5335edb3518d_.py
+++ migrations/versions/5335edb3518d_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: ec5c7296c3b1
+Revision ID: 5335edb3518d
 Revises: 
-Create Date: 2023-01-18 02:26:47.582891
+Create Date: 2023-01-21 00:58:31.136854
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'ec5c7296c3b1'
+revision = '5335edb3518d'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -105,7 +105,6 @@ def upgrade():
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('like_user_id', sa.Integer(), nullable=False),
     sa.Column('post_id', sa.Integer(), nullable=True),
-    sa.ForeignKeyConstraint(['like_user_id'], ['user.id'], ),
     sa.ForeignKeyConstraint(['post_id'], ['post.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
