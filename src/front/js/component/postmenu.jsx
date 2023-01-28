7357bdf4c5e603e345e29c429b20ce043bfe4b27
Hon07 <hangulo14@gmail.com>
2023-01-28 21:09:35 +0000

    No mas fontawesome error

--- src/front/js/component/postmenu.jsx
+++ src/front/js/component/postmenu.jsx
@@ -1,45 +1,45 @@
 import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEllipsisH, faBookmark, faLink, faBell, faEyeSlash, faFlagSwallowtail } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faEllipsisH, faBookmark, faLink, faBell, faEyeSlash, faFlagSwallowtail } from "@fortawesome/free-solid-svg-icons";
 
 export const PostMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="post-menu" style={{ position: "relative" }}>
-      <FontAwesomeIcon icon={faEllipsisH} onClick={() => setIsOpen(!isOpen)} />
-      {isOpen && (
-        <div className="post-dropdown-menu">
-          <div className="post-menu-item">
-            <a href="#">
-              <FontAwesomeIcon icon={faBookmark} /> Save Video
-            </a>
-          </div>
-          <hr />
-          <div className="post-menu-item">
-            <a href="#">
-              <FontAwesomeIcon icon={faLink} /> Copy Link
-            </a>
-          </div>
-          <div className="post-menu-item">
-            <a href="#">
-              <FontAwesomeIcon icon={faBell} /> Turn on notifications
-            </a>
-          </div>
-          <hr />
-          <div className="post-menu-item">
-            <a href="#">
-              <FontAwesomeIcon icon={faEyeSlash} /> Hide Post
-            </a>
-          </div>
-          <div className="post-menu-item">
-            <a href="#">
-              <FontAwesomeIcon icon={faFlagSwallowtail} /> Report Post
-            </a>
-          </div>
+    <i className="fas fa-ellipsis-h" onClick={() => setIsOpen(!isOpen)} />
+    {isOpen && (
+      <div className="post-dropdown-menu">
+        <div className="post-menu-item">
+          <a href="#">
+            <i className="fas fa-bookmark" /> Save Video
+          </a>
         </div>
-      )}
-    </div>
+        <hr />
+        <div className="post-menu-item">
+          <a href="#">
+            <i className="fas fa-link" /> Copy Link
+          </a>
+        </div>
+        <div className="post-menu-item">
+          <a href="#">
+            <i className="fas fa-bell" /> Turn on notifications
+          </a>
+        </div>
+        <hr />
+        <div className="post-menu-item">
+          <a href="#">
+            <i className="fas fa-eye-slash" /> Hide Post
+          </a>
+        </div>
+        <div className="post-menu-item">
+          <a href="#">
+            <i className="fas fa-flag-checkered" /> Report Post
+          </a>
+        </div>
+      </div>
+    )}
+  </div>
   );
 };
 
