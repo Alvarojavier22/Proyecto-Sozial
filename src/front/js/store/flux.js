fda2011d546e76cb8cb7dc6ce365aac147175a9d
Alvaro Javier <alvarojavierchagas@hotmail.com>
2023-01-29 19:24:15 +0000

    Detalles

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,112 +1,105 @@
-const apiUrl = process.env.BACKEND_URL
+const apiUrl = process.env.BACKEND_URL;
 
-const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
+const getState = ({
+    getStore,
+    getActions,
+    setStore
+}) => {
+    return {
+        store: {
+            message: null,
+            demo: [{
+                title: "FIRST",
+                background: "white",
+                initial: "white",
+            },
+            {
+                title: "SECOND",
+                background: "white",
+                initial: "white",
+            },
+            ],
+        },
+        actions: {
+            signUp: async (user) => {
+                let result = await fetch(`${apiUrl}/api/signup/`, {
+                    method: "POST",
+                    body: JSON.stringify(user),
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Origin": "*",
+                    },
+                });
+                result = await result.json();
+                console.log("result", result);
+            },
 
+            GeneratePost: async (post) => {
+                let result = await fetch(`${apiUrl}/api/posts/`, {
+                    method: "POST",
+                    body: JSON.stringify(post),
+                    headers: {
+                        Authorization: "Bearer " +
+                            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY3NDYwOTg1NSwianRpIjoiN2NiY2ZkNDUtMjUzNC00NjE3LWEwNmYtOGJjNGU3MjcyNGFlIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MiwibmJmIjoxNjc0NjA5ODU1LCJleHAiOjE2NzQ2MTA3NTUsInJvbGUiOiJ1c2VyIn0.ZRvPrfk-b3FcepDch37KxL_kpZw5g9zBI_t6UtSUmJg",
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Origin": "*",
+                    },
+                });
+                result = await result.json();
+                console.log("result", result);
+            },
 
-			signUp: async (user)=>{
-				let result=await fetch(`${apiUrl}/api/signup/`,{
-					method: "POST",
-					body: JSON.stringify(user),
-					headers:{
-						"Content-Type": "application/json",
-						"Access-Control-Allow-Origin": "*"
+            login: async (user) => {
+                let result = await fetch(`${apiUrl}/api/login/`, {
+                    method: "POST",
+                    body: JSON.stringify(user),
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Origin": "*",
+                    },
+                });
+                result = await result.json();
+                console.log("result", result);
+            },
 
-					}
-				})
-				result= await result.json()
-				console.log("result", result)
-			},
+            // Use getActions to call a function within a fuction
+            exampleFunction: () => {
+                getActions().changeColor(0, "green");
+            },
 
+            getMessage: async () => {
+                try {
+                    // fetching data from the backend
+                    const resp = await fetch(apiUrl);
+                    const data = await resp.json();
+                    setStore({
+                        message: data.message
+                    });
+                    // don't forget to return something, that is how the async resolves
+                    return data;
+                } catch (error) {
+                    console.log("Error loading message from backend", error);
+                }
+                //console.log(process.env.BACKEND_URL)
+            },
+            changeColor: (index, color) => {
+                //get the store
+                const store = getStore();
 
-			GeneratePost: async(post)=>{
-				let result=await fetch(`${apiUrl}/api/posts/`,{
-					method: "POST",
-					body: JSON.stringify(post),
-					headers:{
-						"Authorization": "Bearer " +"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY3NDYwOTg1NSwianRpIjoiN2NiY2ZkNDUtMjUzNC00NjE3LWEwNmYtOGJjNGU3MjcyNGFlIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MiwibmJmIjoxNjc0NjA5ODU1LCJleHAiOjE2NzQ2MTA3NTUsInJvbGUiOiJ1c2VyIn0.ZRvPrfk-b3FcepDch37KxL_kpZw5g9zBI_t6UtSUmJg",
-						"Content-Type": "application/json",
-						"Access-Control-Allow-Origin": "*"
-					}
-				})
-				result= await result.json()
-				console.log("result", result)
-			},
+                //we have to loop the entire demo array to look for the respective index
+                //and change its color
+                const demo = store.demo.map((elm, i) => {
+                    if (i === index) elm.background = color;
+                    return elm;
+                });
 
-			login: async(user)=>{
-				let result=await fetch(`${apiUrl}/api/login/`,{
-				method: "POST",
-				body: JSON.stringify(user),
-				headers:{
-					"Content-Type": "application/json",
-					"Access-Control-Allow-Origin": "*"
-				}
-
-				})
-				result= await result.json()
-				console.log("result", result)
-			},
-
-
-
-
-
-
-
-
-
-
-
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(apiUrl)
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-				//console.log(process.env.BACKEND_URL)
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+                //reset the global store
+                setStore({
+                    demo: demo
+                });
+            },
+        },
+    };
 };
 
 export default getState;
