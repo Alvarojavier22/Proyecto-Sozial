6953db6a2297bd212d4b738afb94c4fee864be74
Alvaro Javier <alvarojavierchagas@hotmail.com>
2023-01-29 20:16:18 +0000

    Mas detalles

--- src/front/js/component/ProfileNav.jsx
+++ src/front/js/component/ProfileNav.jsx
@@ -12,11 +12,11 @@ export const ProfileNav = () => {
                 width="36"
                 height="36"
                 fill="currentColor"
-                class="bi bi-arrow-left-short"
+                className="bi bi-arrow-left-short"
                 viewBox="0 0 16 16"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"
                 />
               </svg>
@@ -28,14 +28,14 @@ export const ProfileNav = () => {
                 src="https://wl-genial.cf.tsp.li/resize/728x/jpg/91b/430/964a9c5ac9933cc012d0bd80be.jpg"
               />
             </div>
-            <form class="d-flex" style={{ marginLeft: "20px", width: "400px" }}>
+            <form className="d-flex" style={{ marginLeft: "20px", width: "400px" }}>
               <input
-                class="form-control me-2"
+                className="form-control me-2"
                 type="search"
                 placeholder="Search on sozial"
                 aria-label="Search"
               />
-              <button class="btn btn-outline-success" type="submit">
+              <button className="btn btn-outline-success" type="submit">
                 Search
               </button>
             </form>
@@ -46,7 +46,7 @@ export const ProfileNav = () => {
               width="24"
               height="24"
               fill="currentColor"
-              class="bi bi-chat-left-dots"
+              className="bi bi-chat-left-dots"
               viewBox="0 0 16 16"
             >
               <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
@@ -58,7 +58,7 @@ export const ProfileNav = () => {
                 width="22"
                 height="22"
                 fill="currentColor"
-                class="bi bi-share"
+                className="bi bi-share"
                 viewBox="0 0 16 16"
               >
                 <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z" />
