8a0079fffc4ede1a05f87c457e394d495b080449
alejandropmz <alejandropacheco2497@gmail.com>
2023-02-02 03:50:57 +0000

    igualar estilos

--- src/front/js/component/ProfileInfo.jsx
+++ src/front/js/component/ProfileInfo.jsx
@@ -3,17 +3,26 @@ import React from "react";
 export const ProfileInfo = (props) => {
   return (
     <div className="container-fluid ProfileInfo-container">
-      <img src="https://wl-genial.cf.tsp.li/resize/728x/jpg/91b/430/964a9c5ac9933cc012d0bd80be.jpg" />
-      <div className="editButton">Edit profile</div>
-      <h4>{props.username}</h4>
-      <div className="followers">
+      <div className="d-flex justify-content-center">
+        <img src="https://wl-genial.cf.tsp.li/resize/728x/jpg/91b/430/964a9c5ac9933cc012d0bd80be.jpg" />
+      </div>
+      <div className="d-flex justify-content-center but">
+        <button className="btn btn-primary">Edit profile</button>
+      </div>
+      <div className="d-flex justify-content-center h4">
+        <h4>{props.username}</h4>
+      </div>
+      <div className="followers d-flex justify-content-center">
         <strong>
           <p>{props.followersNumber} followers</p>
         </strong>
       </div>
-      <small>
-        {props.username} - {props.postsNumber} posts - {props.joinDate}
-      </small>
+      <div className="d-flex justify-content-center">
+        <small>
+          {props.postsNumber} posts - {props.joinDate}
+        </small>
+        <small></small>
+      </div>
     </div>
   );
 };
