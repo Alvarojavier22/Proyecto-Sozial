0343fa69191d23bbcfbcc47e09523129f372aeab
alejandropmz <alejandropacheco2497@gmail.com>
2023-02-09 15:03:51 +0000

    rehacer el componente ProductCart

--- src/front/js/pages/Cart2.jsx
+++ src/front/js/pages/Cart2.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { InfoNav } from "../component/InfoNav.jsx";
+import { ProductCart } from "../component/ProductCart.jsx";
 import { ProfileNav } from "../component/ProfileNav.jsx";
 
 export const CartTwo = () => {
@@ -18,104 +19,7 @@ export const CartTwo = () => {
                 <p>Items are reserved for only 60 minutes</p>
               </div>
             </div>
-            <div
-              style={{ border: "orange solid 2px" }}
-              className=" shopping-details-content"
-            >
-              <div
-                style={{ border: "blue solid 2px" }}
-                className="shopping-details-content-img"
-              >
-                <img src="https://http2.mlstatic.com/D_NQ_NP_629893-CBT48890389810_012022-O.jpg" />
-              </div>
-              <div
-                style={{ border: "red solid 2px" }}
-                className="shopping-details-content-description"
-              >
-                <div className="content-description-price">
-                  <div>
-                    <p>$20.00</p>
-                  </div>
-                  <div>
-                    <i className="bi bi-x"></i>
-                  </div>
-                </div>
-                <div className="content-description-specifications">
-                  <p>OTAKU DESIGN Slim mid lenght start t-shirt</p>
-                  <p>Black Silk</p>
-                </div>
-                <div className="buttons-specifications">
-                  <div>
-                    <p>Black Silk</p>
-                  </div>
-                  {/* mapear estos botones */}
-                  <div className="borders">
-                    <div className="btn-group" role="group">
-                      <button
-                        id="btnGroupDrop1"
-                        type="button"
-                        className="btn btn-muted dropdown-toggle"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                      >
-                        Dropdown
-                      </button>
-                      <ul
-                        className="dropdown-menu"
-                        aria-labelledby="btnGroupDrop1"
-                      >
-                        <li>
-                          <a className="dropdown-item" href="#">
-                            Dropdown link
-                          </a>
-                        </li>
-                        <li>
-                          <a className="dropdown-item" href="#">
-                            Dropdown link
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div>
-                    <div className="btn-group" role="group">
-                      <button
-                        id="btnGroupDrop1"
-                        type="button"
-                        className="btn btn-muted dropdown-toggle"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                      >
-                        Quantity
-                      </button>
-                      <ul
-                        className="dropdown-menu"
-                        aria-labelledby="btnGroupDrop1"
-                      >
-                        <li>
-                          <a className="dropdown-item" href="#">
-                            1
-                          </a>
-                        </li>
-                        <li>
-                          <a className="dropdown-item" href="#">
-                            2
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-                <div className="favorite-button">
-                  <button className="btn btn-info">
-                    <div>
-                      <i className="bi bi-heart"></i>
-                    </div>
-                    <div>For later</div>
-                  </button>
-                </div>
-              </div>
-            </div>
+            <ProductCart />
             <div className="shopping-details-footer d-flex justify-content-end">
               <div className="footer-price d-flex">
                 <h4>Sub Total:</h4>
