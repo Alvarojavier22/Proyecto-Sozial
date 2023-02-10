428f439a3773697165ec267766fb519300f05866
Alvaro Javier <alvarojavierchagas@hotmail.com>
2023-02-10 18:10:00 +0000

    Listo navbar

--- src/front/js/component/navbarfeed.jsx
+++ src/front/js/component/navbarfeed.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-export const NavabarFeed = () => {
+
+export const NavBarFeed = () => {
   const [userData, setUserData] = useState({});
 
   useEffect(() => {
@@ -8,14 +9,10 @@ export const NavabarFeed = () => {
   }, []);
   return (
     <div className="container-fluid">
-      <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: "#FFFFFF" }}>
-        <div className="container-fluid">
+       <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid profile-nav">          
+        <div className="d-flex">
           <span className="navbar-brand">Sozial</span>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="container-fluid profile-nav">
-            <div className="d-flex">
               <div className="photo-arrow" style={{ marginLeft: "20px" }}>
                 <img
                   style={{ width: "45px", height: "45px", borderRadius: "100%" }}
@@ -103,10 +100,8 @@ export const NavabarFeed = () => {
                 </li>
               </ul>
             </div>
-          </div>
-        </div>
+         </div>
       </nav>
-    </div>
-  );
+    </div>  );
 }
 
--- src/front/js/pages/feed.jsx
+++ src/front/js/pages/feed.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import  { NavabarFeed } from "../component/navabarfeed.jsx";
+import  { NavBarFeed } from "../component/navbarfeed.jsx";
 import { LeftMenu } from "../component/leftmenu.jsx";
 import { PostFeed } from "../component/postfeed.jsx";
 import "../../styles/index.css"
@@ -10,7 +10,7 @@ import  {PostMenu} from "../component/postmenu.jsx";
 export const Feed = () => {
     return (
       <div className="feed-container">
-        <div className="feedheader row"><NavabarFeed/></div>
+        <div className="feedheader row"><NavBarFeed/></div>
         <div className="row feedcont mt-5">
         <div className="col-2 mt-5 me-5 p-2 d-none d-md-block"><LeftMenu/> </div>
         <div className="col "> <PostFeed/></div>
