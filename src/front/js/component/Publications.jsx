f3d6ce721dbcae094f7765b3f7febac3f0fcb50f
alejandropmz <alejandropacheco2497@gmail.com>
2023-02-13 17:22:11 +0000

    desplegable comment

--- src/front/js/component/Publications.jsx
+++ src/front/js/component/Publications.jsx
@@ -2,9 +2,14 @@ import React, { useState } from "react";
 
 export const Publications = (props) => {
   const [like, setLike] = useState(false);
+  const [comment, setComment] = useState(false);
 
   const Like = () => {
     setLike(!like);
+  };
+
+  const Comment = () => {
+    setComment(!comment);
   };
 
   return (
@@ -57,8 +62,15 @@ export const Publications = (props) => {
                   ></i>
                   <p>Like</p>
                 </button>
-                <button className="btn btn-primary pub-buttons d-flex">
-                  <i className="bi bi-chat-dots"></i>
+                <button
+                  onClick={Comment}
+                  className="btn btn-primary pub-buttons d-flex"
+                >
+                  <i
+                    className={
+                      comment ? "bi bi-chat-dots-fill" : "bi bi-chat-dots"
+                    }
+                  ></i>
                   <p>Comment</p>
                 </button>
                 <button className="btn btn-primary pub-buttons d-flex">
@@ -68,31 +80,35 @@ export const Publications = (props) => {
               </div>
             </div>
             <div className="post-divider"></div>
-            <div className="input-comment">
-              <img src={props.onlineUserPhoto} />
-              <div className="input-group mb-3">
-                <input
-                  placeholder="comment here"
-                  type="text"
-                  className="form-control"
-                  aria-label="Sizing example input"
-                  aria-describedby="inputGroup-sizing-sm"
-                />
+            {comment ? (
+              <div className="input-comment">
+                <img src={props.onlineUserPhoto} />
+                <div className="input-group mb-3">
+                  <input
+                    placeholder="comment here"
+                    type="text"
+                    className="form-control"
+                    aria-label="Sizing example input"
+                    aria-describedby="inputGroup-sizing-sm"
+                  />
+                </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="36"
+                  height="36"
+                  fill="currentColor"
+                  className="bi bi-arrow-right-square post-arrow"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
+                  />
+                </svg>
               </div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="36"
-                height="36"
-                fill="currentColor"
-                className="bi bi-arrow-right-square post-arrow"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
-                />
-              </svg>
-            </div>
+            ) : (
+              ""
+            )}
           </div>
         </div>
       </div>
