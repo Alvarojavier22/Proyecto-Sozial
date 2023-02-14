0fe64758bd6b3a755d65baee37d913b15bb2084d
alejandropmz <alejandropacheco2497@gmail.com>
2023-02-14 03:19:34 +0000

    desplegable de publicar productos

--- src/front/js/pages/PostProduct2.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-
-import { InfoNav } from "../component/InfoNav.jsx";
-import { ProfileNav } from "../component/ProfileNav.jsx";
-
-export const PostProduct2 = () => {
-  return <h1>hi</h1>;
-};
