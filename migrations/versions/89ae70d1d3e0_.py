1fd68d751cfb3cea1391a06e5938c453a1fd12e3
alejandropmz <alejandropacheco2497@gmail.com>
2023-02-15 00:05:30 +0000

    commt

--- migrations/versions/89ae70d1d3e0_.py
+++ migrations/versions/89ae70d1d3e0_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: a1ef932b3a0c
+Revision ID: 89ae70d1d3e0
 Revises: 
-Create Date: 2023-02-14 03:22:21.128671
+Create Date: 2023-02-14 15:57:47.685552
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'a1ef932b3a0c'
+revision = '89ae70d1d3e0'
 down_revision = None
 branch_labels = None
 depends_on = None
