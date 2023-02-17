c2b794d965b2aa6dc4ee1b72d55d71b038f41705
Alvaro Javier <alvarojavierchagas@hotmail.com>
2023-02-17 02:39:37 +0000

    key value

--- migrations/versions/79988ca25b7d_.py
+++ migrations/versions/79988ca25b7d_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: e82404d5a5ac
+Revision ID: 79988ca25b7d
 Revises: 
-Create Date: 2023-02-17 00:49:42.305850
+Create Date: 2023-02-17 02:37:43.863977
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'e82404d5a5ac'
+revision = '79988ca25b7d'
 down_revision = None
 branch_labels = None
 depends_on = None
