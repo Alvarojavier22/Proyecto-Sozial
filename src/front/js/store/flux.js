a5a8cb674b41e348f9e854f95447e16506d1650e
alejandropmz <alejandropacheco2497@gmail.com>
2023-02-18 05:53:56 +0000

    algunos cambios

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -129,6 +129,7 @@ const getState = ({ getStore, getActions, setStore }) => {
       posts: [
         {
           id: 1,
+          likesNumber: 2,
           img: "https://i.pinimg.com/736x/60/25/69/6025694842b457c914461e991349d7a7.jpg",
           text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
           username: "Carl Jhonson",
@@ -138,6 +139,7 @@ const getState = ({ getStore, getActions, setStore }) => {
         },
         {
           id: 2,
+          likesNumber: 17,
           img: "https://fastly.picsum.photos/id/923/600/600.jpg?hmac=-Z9u__nla6z2tlApM048LeVj04Bf2U4q4I8dbynZPCc",
           text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem",
           username: "Suset Vera",
@@ -154,6 +156,7 @@ const getState = ({ getStore, getActions, setStore }) => {
         },
         {
           id: 4,
+          likesNumber: 22,
           img: "https://fastly.picsum.photos/id/504/600/600.jpg?hmac=8PjIqVUS8HTj2LXiFmTyENGerLlisWJqREkmq686c0U",
           text: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour",
           username: "Adalberto Peñaranda",
@@ -163,6 +166,7 @@ const getState = ({ getStore, getActions, setStore }) => {
         },
         {
           id: 5,
+          likesNumber: 48,
           img: "https://fastly.picsum.photos/id/318/600/600.jpg?hmac=EMEdyMWaWsfIAfLKAtHC8CEyjWYYd446gDsGJ81NZA4",
           text: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, comes from a line in section 1.10.32.",
           username: "Daysi Dawson",
